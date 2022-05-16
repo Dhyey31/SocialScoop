7c703da42fa0420cacc10be2e2ef407b064fe89d
Dhyey31 <100749784+Dhyey31@users.noreply.github.com>
2022-05-16 12:17:08 +0530

    Delete socialscoop-backend-main directory

--- socialscoop-backend-main/routes/chat_1.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createChat, getChats } = require('../controllers/chat');
-const router = express.Router();
-
-router.route('/:receiverId').post(createChat);
-router.route('/').get(getChats);
-
-module.exports = router;
